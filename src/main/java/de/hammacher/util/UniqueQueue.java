607f2bc083b8b0ba966fc1b2a658db50fc1e89b7
clemens <clemens@664ea281-2b33-0410-be9d-ee02faee088f>
2009-01-20 20:55:48 +0000

    git-svn-id: file:///var/www/clemens/svn/studium@131 664ea281-2b33-0410-be9d-ee02faee088f

--- src/main/java/de/hammacher/util/UniqueQueue.java
+++ src/main/java/de/hammacher/util/UniqueQueue.java
@@ -7,30 +7,47 @@ import java.util.Set;
 
 
 /**
- * A Queue in which every element is only inserted once. Further insertions have
- * no effekt.
+ * A Queue in which every element is only inserted once.
+ *
+ * A boolean flag controls whether elements can be reinserted
+ * after they have been taken out of the queue.
  *
  * @author Clemens Hammacher
  */
 public class UniqueQueue<E> extends ArrayQueue<E> {
+
     private final Set<E> seen;
+    private final boolean allowReinsertion;
 
     public UniqueQueue() {
+    	this(false);
+    }
+
+    public UniqueQueue(boolean allowReinsertion) {
         super();
+        this.allowReinsertion = allowReinsertion;
         this.seen = new HashSet<E>();
     }
 
-    public UniqueQueue(final Collection<? extends E> c) {
+    public UniqueQueue(final Collection<? extends E> c, boolean allowReinsertion) {
         super(c);
+        this.allowReinsertion = allowReinsertion;
         this.seen = new HashSet<E>(c);
     }
 
-    public UniqueQueue(final int numElements) {
-        super(numElements);
-        this.seen = new HashSet<E>(numElements);
+    public UniqueQueue(final int initialCapacity, boolean allowReinsertion) {
+        super(initialCapacity);
+        this.allowReinsertion = allowReinsertion;
+        this.seen = new HashSet<E>(initialCapacity);
     }
 
-    public Set<E> getSeen() {
+    /**
+     * Returns a set of all elements that are not allowed to be reinserted at the moment.
+     * If reinsertion is not allowed, this set contains all elements that have ever been
+     * inserted into the queue, otherwise it contains exactly the elements of this queue.
+     * @return a set of all elements that are not allowed to be reinserted
+     */
+    public Set<E> getSeenElements() {
         return Collections.unmodifiableSet(this.seen);
     }
 
@@ -83,5 +100,53 @@ public class UniqueQueue<E> extends ArrayQueue<E> {
     public void clearSeen() {
         this.seen.clear();
     }
+
+	@Override
+	public void clear() {
+		super.clear();
+		this.seen.clear();
+	}
+
+	@Override
+	public E pollFirst() {
+		E e = super.pollFirst();
+		if (this.allowReinsertion)
+			this.seen.remove(e);
+		return e;
+	}
+
+	@Override
+	public E pollLast() {
+		E e = super.pollLast();
+		if (this.allowReinsertion)
+			this.seen.remove(e);
+		return e;
+	}
+
+	@Override
+	public boolean remove(Object o) {
+		if (!super.remove(o))
+			return false;
+
+		if (this.allowReinsertion)
+			this.seen.remove(o);
+		return true;
+	}
+
+	@Override
+	public E removeFirst() {
+		E e = super.removeFirst();
+		if (this.allowReinsertion)
+			this.seen.remove(e);
+		return e;
+	}
+
+	@Override
+	public E removeLast() {
+		E e = super.removeLast();
+		if (this.allowReinsertion)
+			this.seen.remove(e);
+		return e;
+	}
 
 }
