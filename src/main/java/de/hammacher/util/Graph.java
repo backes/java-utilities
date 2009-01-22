fee44f70a73197fea48eda616e7d804a841b5013
clemens <clemens@664ea281-2b33-0410-be9d-ee02faee088f>
2009-01-22 19:31:14 +0000

    git-svn-id: file:///var/www/clemens/svn/studium@132 664ea281-2b33-0410-be9d-ee02faee088f

---
+++ src/main/java/de/hammacher/util/Graph.java
@@ -0,0 +1,37 @@
+package de.hammacher.util;
+
+import java.util.Collection;
+
+/**
+ * A generic graph interface.
+ *
+ * @author Clemens Hammacher
+ * @param <NodeType> the type of the nodes. must implements {@link Graph.Node}.
+ */
+public interface Graph<NodeType extends Graph.Node<NodeType>> {
+
+	public static interface Node<NodeType extends Node<NodeType>> {
+
+		/**
+		 * Returns a collection of all successors of this node.
+		 * All these successors must be contained in the collection returned
+		 * by <code>getGraph().getNodes()</code>.
+		 *
+		 * @return a collection of all successors of a node
+		 */
+		Collection<? extends NodeType> getSuccessors();
+
+		Graph<NodeType> getGraph();
+
+		String getLabel();
+
+	}
+
+	/**
+	 * Returns a collection of all nodes of this graph.
+	 *
+	 * @return a collection of all nodes of this graph
+	 */
+	Collection<? extends NodeType> getNodes();
+
+}
