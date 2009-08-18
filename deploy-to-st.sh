0b7cf39539576d57f03a700cb79d6b8200479a3b
clemens <clemens@664ea281-2b33-0410-be9d-ee02faee088f>
2009-08-18 13:26:02 +0000

    added deploy-to-st script
    git-svn-id: file:///var/www/clemens/svn/studium@176 664ea281-2b33-0410-be9d-ee02faee088f

---
+++ deploy-to-st.sh
@@ -0,0 +1,25 @@
+#!/bin/sh
+
+version=1.2.8
+
+mvn install
+
+mvn deploy:deploy-file \
+    -Dfile=target/utilities-${version}.jar \
+    -DpomFile=pom.xml \
+    -DrepositoryId=group-internal \
+    -Durl=dav:https://maven.st.cs.uni-saarland.de/nexus/content/repositories/thirdparty
+
+mvn deploy:deploy-file \
+    -Dfile=target/utilities-${version}-sources.jar \
+    -Dclassifier=sources \
+    -DpomFile=pom.xml \
+    -DrepositoryId=group-internal \
+    -Durl=dav:https://maven.st.cs.uni-saarland.de/nexus/content/repositories/thirdparty
+mvn deploy:deploy-file \
+    -Dfile=target/utilities-${version}-javadoc.jar \
+    -Dclassifier=javadoc \
+    -DpomFile=pom.xml \
+    -DrepositoryId=group-internal \
+    -Durl=dav:https://maven.st.cs.uni-saarland.de/nexus/content/repositories/thirdparty
+
