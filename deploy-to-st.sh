d72a33832230ab795d4c3218d08c036eeafe5646
clemens <clemens@664ea281-2b33-0410-be9d-ee02faee088f>
2009-12-30 21:33:29 +0000

    increased version number to 1.2.9 (not released yet)
    git-svn-id: file:///var/www/clemens/svn/studium@187 664ea281-2b33-0410-be9d-ee02faee088f

--- deploy-to-st.sh
+++ deploy-to-st.sh
@@ -2,24 +2,24 @@
 
 version=1.2.8
 
-mvn install
+#mvn install
 
 mvn deploy:deploy-file \
     -Dfile=target/utilities-${version}.jar \
     -DpomFile=pom.xml \
-    -DrepositoryId=group-internal \
+    -DrepositoryId=st-thirdparty \
     -Durl=dav:https://maven.st.cs.uni-saarland.de/nexus/content/repositories/thirdparty
 
 mvn deploy:deploy-file \
     -Dfile=target/utilities-${version}-sources.jar \
     -Dclassifier=sources \
     -DpomFile=pom.xml \
-    -DrepositoryId=group-internal \
+    -DrepositoryId=st-thirdparty \
     -Durl=dav:https://maven.st.cs.uni-saarland.de/nexus/content/repositories/thirdparty
 mvn deploy:deploy-file \
     -Dfile=target/utilities-${version}-javadoc.jar \
     -Dclassifier=javadoc \
     -DpomFile=pom.xml \
-    -DrepositoryId=group-internal \
+    -DrepositoryId=st-thirdparty \
     -Durl=dav:https://maven.st.cs.uni-saarland.de/nexus/content/repositories/thirdparty
 
