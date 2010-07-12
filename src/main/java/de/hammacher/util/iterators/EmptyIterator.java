7e92e3ed880cc672894efe67a1d6834a1b9c5514
clemens <clemens@664ea281-2b33-0410-be9d-ee02faee088f>
2010-07-12 14:29:04 +0000

    EmptyIterator now implements ListIterator
    git-svn-id: file:///var/www/clemens/svn/studium@200 664ea281-2b33-0410-be9d-ee02faee088f

--- src/main/java/de/hammacher/util/iterators/EmptyIterator.java
+++ src/main/java/de/hammacher/util/iterators/EmptyIterator.java
@@ -1,28 +1,66 @@
 package de.hammacher.util.iterators;
 
-import java.util.Iterator;
+import java.util.ListIterator;
 import java.util.NoSuchElementException;
 
-public class EmptyIterator<T> implements Iterator<T> {
+public class EmptyIterator<T> implements ListIterator<T> {
 
 	private static final EmptyIterator<?> instance =
 		new EmptyIterator<Object>();
+
+	private EmptyIterator() {
+	    // not instantiable
+	}
 
 	@SuppressWarnings("unchecked")
 	public static <T> EmptyIterator<T> getInstance() {
 		return (EmptyIterator<T>) instance;
 	}
 
+    @Override
     public boolean hasNext() {
         return false;
     }
 
+    @Override
     public T next() {
         throw new NoSuchElementException();
     }
 
+    @Override
     public void remove() {
         throw new IllegalStateException();
+    }
+
+    @Override
+    public boolean hasPrevious() {
+        return false;
+    }
+
+    @Override
+    public T previous() {
+        throw new NoSuchElementException();
+    }
+
+    @Override
+    public int nextIndex() {
+        return 0;
+    }
+
+    @Override
+    public int previousIndex() {
+        return -1;
+    }
+
+    @Override
+    public void set(T e) {
+        throw new IllegalStateException();
+    }
+
+    @Override
+    public void add(T e) {
+        throw new UnsupportedOperationException("Cannot add elements to an " +
+            EmptyIterator.class.getSimpleName());
     }
 
 }
